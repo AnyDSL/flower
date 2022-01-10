af1a7dd0530b245fb7a9279799aace1cb0df72b6
Puya Amiri <pooya.w@gmail.com>
2022-01-10 20:21:32 +0100

    update depricated script

--- scripts/board/Xilinx/init_xilinx.sh
+++ scripts/board/Xilinx/init_xilinx.sh
@@ -1,10 +1,8 @@
-mode=$1
-#export SELECTED_BOARD=zcu
-export SELECTED_VERSION=2018.2
-path="/opt/Xilinx"
-source $path/Vivado/${SELECTED_VERSION}/settings64.sh
-if [ "$1" = "soc" ];
-then
-    echo "SDSoC got initialized"
-    source $path/SDx/${SELECTED_VERSION}/settings64.sh
-fi
+vitis_path=$1
+vitis_ver=$2
+
+export SELECTED_VERSION=2020.1
+path="/opt/tools/xilinx"
+
+source $path/Vitis/${SELECTED_VERSION}/settings64.sh
+source /opt/xilinx/xrt/setup.sh
